7946fb746310cb6c5b41f2743df8979f027b1427
Batuhan Wilhelm <batuhanwilhelm@gmail.com>
2021-03-27 02:13:50 +0300

    Generate global.d.ts

--- src/index.ts
+++ src/index.ts
@@ -1,28 +1,71 @@
 import { Command, flags } from '@oclif/command';
+import * as path from 'path';
+import * as fs from 'fs';
+import * as Mustache from 'mustache';
 
 class GenerateGlobalDTs extends Command {
   static description = 'describe the command here';
 
   static flags = {
-    // add --version flag to show CLI version
     version: flags.version({ char: 'v' }),
     help: flags.help({ char: 'h' }),
-    // flag with a value (-n, --name=VALUE)
-    name: flags.string({ char: 'n', description: 'name to print' }),
-    // flag with no value (-f, --force)
-    force: flags.boolean({ char: 'f' }),
   };
 
-  static args = [{ name: 'file' }];
+  static args = [{ name: 'file', description: 'path to env file. defaults to .env' }];
 
   async run() {
-    const { args, flags } = this.parse(GenerateGlobalDTs);
+    const { args } = this.parse(GenerateGlobalDTs);
 
-    const name = flags.name ?? 'world';
-    this.log(`hello ${name} from ./src/index.ts`);
-    if (args.file && flags.force) {
-      this.log(`you input --force and --file: ${args.file}`);
+    const envPath = args.file ?? '.env';
+
+    this.log(`getting variables from ${envPath}`);
+
+    const envVariables = this.getEnvVariables(envPath);
+
+    const filePath = path.join(process.cwd(), 'global.d.ts');
+
+    if (!fs.existsSync(filePath)) {
+      fs.writeFileSync(filePath, ``);
     }
+
+    const file = this.renderFile(envVariables);
+
+    fs.writeFileSync(path.resolve(process.cwd(), 'global.d.ts'), file);
+
+    this.log('successfully generated');
+  }
+
+  getEnvVariables(envPath: string) {
+    const envVariables: string[] = [];
+
+    fs.readFileSync(path.join(process.cwd(), envPath), 'utf-8')
+      .split(/\r?\n/)
+      .forEach((line: string) => {
+        if (line) envVariables.push(line.split('=')[0]);
+      });
+
+    return envVariables;
+  }
+
+  renderFile(envVariables: string[]) {
+    return Mustache.render(this.template(), {
+      variables: envVariables
+        .filter((e) => e !== 'NODE_ENV' && e !== 'PORT')
+        .map((e) => ({ variableName: e })),
+    });
+  }
+
+  template() {
+    return `declare namespace NodeJS {
+      interface ProcessEnv {
+        NODE_ENV: 'development' | 'test' | 'staging' | 'production';
+        PORT: string;
+        {{#variables}}
+        {{variableName}}: string;
+        {{/variables}}
+      }
+    }
+    `;
   }
 }
 
